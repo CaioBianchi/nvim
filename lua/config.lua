bd184ad53d8a26985dc68cde60768003be897414
Caio Bianchi <caio@caio.ca>
2021-11-29 20:52:09 -0500

    And indentation config

--- lua/config.lua
+++ lua/config.lua
@@ -33,9 +33,38 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.wo.colorcolumn = "99999"
+vim.g.indent_blankline_context_patterns = {
+  "class",
+  "return",
+  "function",
+  "method",
+  "^if",
+  "^while",
+  "jsx_element",
+  "^for",
+  "^object",
+  "^table",
+  "block",
+  "arguments",
+  "if_statement",
+  "else_clause",
+  "jsx_element",
+  "jsx_self_closing_element",
+  "try_statement",
+  "catch_clause",
+  "import_statement",
+  "operation_type",
+}
 
 require'indent_blankline'.setup {
-  char = "|", 
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
+  use_treesitter = true,
   buftype_exclude = {"terminal"} 
 }
 
